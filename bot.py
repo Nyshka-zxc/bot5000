8328ea5286c36ac713fa3b5168e878f7d21cb47d
Nyshka-zxc <maxim.muksunov@gmail.com>
2026-08-23 19:16:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1754,6 +1754,7 @@ def process_command(event):
             return
         db_update_user(user_id, btc=row[3] - 500, protection=1)
         send_message(event.peer_id, "✅ Защита от кражи куплена!")
+        
 
         elif command == 'стикеры':
         target_id, _ = get_target_and_clean_args(event, args)
